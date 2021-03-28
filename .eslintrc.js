1d56af251abc63020c092148ce4b4af4b75ecd7b
Lucas Boucher <lucas@sublissi.me>
2021-03-28 19:13:57 +0200

    Removed Prettier in favor of purely eslint config. Added custom eslint rules
    Changed displayName to "Jude Plato"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'linebreak-style': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    indent: ['error', 2, { MemberExpression: 0 }],
+    'global-require': ['off'],
+    'react/jsx-one-expression-per-line': 'off',
+  },
 };
