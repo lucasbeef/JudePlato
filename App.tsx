2204e8cf01e5372cda421b7d37dd5210393ea14e
Lucas Boucher <lucas@sublissi.me>
2021-03-31 15:46:16 +0200

    New container PlayerSelection that takes a PlayerType[] and returns a FlatList of the ProfilePicture each embedded in a PositionButton
    Tried it in the App component with dummy data

--- App.tsx
+++ App.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
 
-const App: React.FC = () => (
-  <SafeAreaView>
-    <StatusBar barStyle="light-content" />
-    <ScrollView contentInsetAdjustmentBehavior="automatic" />
-  </SafeAreaView>
-);
+import PlayerSelection from './app/containers/PlayerSelection';
+import { selectPlayerAction, deselectPlayerAction } from './app/actions/playersActions';
+import { PlayerType } from './app/types/PlayerType';
+
+const DUMMY_PLAYER_STATE = [{
+  id: 0,
+  firstName: 'Gilbert',
+  image: 'https://randomuser.me/api/portraits/men/22.jpg',
+  position: -1,
+}, {
+  id: 1,
+  firstName: 'Jean-Louis',
+  image: 'https://randomuser.me/api/portraits/men/12.jpg',
+  position: -1,
+}, {
+  id: 2,
+  firstName: 'Lucienne',
+  image: 'https://randomuser.me/api/portraits/women/12.jpg',
+  position: -1,
+}, {
+  id: 3,
+  firstName: 'Irène-Louis-Marie',
+  image: 'https://randomuser.mer/api/portraits/women/47.jpg',
+  position: -1,
+}];
+
+const App = () => {
+  const [players, setPlayers] = useState<PlayerType[]>(DUMMY_PLAYER_STATE);
+
+  const onPlayerPress = (passedPlayers: PlayerType[], id: number) => {
+    const pressedPlayer = players.find((player) => player.id === id);
+
+    if (!pressedPlayer) {
+      throw new Error('Player id was not found in player state');
+    }
+
+    if (pressedPlayer.position === -1) {
+      setPlayers(selectPlayerAction(passedPlayers, id));
+    }
+    else {
+      setPlayers(deselectPlayerAction(passedPlayers, id));
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="light-content" />
+      <Text>Select your players</Text>
+      <PlayerSelection players={players} onPlayerPress={onPlayerPress} />
+    </SafeAreaView>
+  );
+};
 
 export default App;
---
+++ app/containers/PlayerSelection.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+import PositionButton from '../components/PositionButton';
+import ProfilePicture from '../components/ProfilePicture';
+import { PlayerType } from '../types/PlayerType';
+
+const styles = StyleSheet.create({
+  columnWrapper: {
+    justifyContent: 'space-between',
+  },
+});
+
+type PropsType = {
+  players: PlayerType[],
+  onPlayerPress: (players: PlayerType[], id: number) => void,
+}
+
+/* TODO make the component generic using a renderProp or hook */
+const PlayerSelection = ({ players, onPlayerPress }: PropsType) => {
+  const renderPlayer = (player: PlayerType) => (
+    <PositionButton
+      onPress={() => onPlayerPress(players, player.id)}
+      position={player.position}
+    >
+      <ProfilePicture image={player.image} name={player.firstName} />
+    </PositionButton>
+  );
+
+  return (
+    <FlatList
+      data={players}
+      numColumns={4}
+      columnWrapperStyle={styles.columnWrapper}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => renderPlayer(item)}
+    />
+  );
+};
+
+export default PlayerSelection;
